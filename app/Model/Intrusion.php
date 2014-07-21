77320ff087ec7c45f492fa30202f0c1b62cd76e2
Steve <rainoftyren@gmail.com>
2014-07-20 17:28:36 -0700

    Brought Intrusion and Tag models up to speed.

--- app/Model/Intrusion.php
+++ app/Model/Intrusion.php
@@ -1,5 +1,24 @@
 <?php
 
 class Intrusion extends AppModel {
-  
+  public $order = "Intrusion.created_at DESC";
+  public $validate = array(
+    'description' => array(
+      'isUnique' => array(
+        'rule' => 'isUnique',
+        'message' => 'Another intrusion with this description already exists.'
+      ),
+      'notEmpty' => array(
+        'rule' => 'notEmpty',
+        'message' => 'Description cannot be empty.'
+      )
+    )
+  );
+  public $hasAndBelongsToMany = array(
+    'Tag' => array(
+      'className' => 'Tag',
+      'unique' => 'keepExisting',
+      'order' => 'name'
+    )
+  );
 }
---
+++ app/Model/Tag.php
@@ -0,0 +1,24 @@
+<?php
+
+class Tag extends AppModel {
+  public $order = "name";
+  public $validate = array(
+    'name' => array(
+      'isUnique' => array(
+        'rule' => 'isUnique',
+        'message' => 'Another tag with this name already exists.'
+      ),
+      'notEmpty' => array(
+        'rule' => 'notEmpty',
+        'message' => 'Name cannot be empty.'
+      )
+    )
+  );
+  public $hasAndBelongsToMany = array(
+    'Intrusion' => array(
+      'className' => 'Intrusion',
+      'unique' => 'keepExisting',
+      'order' => 'Intrusion.created_at DESC'
+    )
+  );
+}
