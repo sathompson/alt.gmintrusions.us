e038f79a6f9743089a05c18bc1a22327ef453483
Steve <rainoftyren@gmail.com>
2014-07-20 19:04:24 -0700

    Added view action to IntrusionsController and updated index view to add links to view.

--- app/Controller/IntrusionsController.php
+++ app/Controller/IntrusionsController.php
@@ -6,4 +6,30 @@ class IntrusionsController extends AppController {
   public function index() {
     $this->set('intrusions', $this->Intrusion->find('all'));
   }
+
+  public function view($id = null) {
+    $this->setIntrusion($id);
+  }
+
+
+
+  private function setIntrusion($id = null) {
+    if (!$id) {
+      throw new NotFoundException(__('Invalid intrusion'));
+    }
+
+    $intrusion = $this->Intrusion->findById($id);
+
+    if (!$intrusion) {
+      throw new NotFoundException(__('Invalid intrusion'));
+    }
+
+    $this->set('intrusion', $intrusion);
+  }
+
+  private function flashSuccess($msg = null) {
+    if ($msg) {
+      $this->Session->setFlash($msg, 'success_flash');
+    }
+  }
 }
--- app/View/Intrusions/index.ctp
+++ app/View/Intrusions/index.ctp
@@ -1,6 +1,6 @@
 <h1>Intrusions</h1>
 <?php foreach ($intrusions as $intrusion): ?>
   <hr>
-  <p><?php echo $intrusion['Intrusion']['description']; ?></p>
+  <p><?php echo $this->Html->link($intrusion['Intrusion']['description'], array('controller' => 'intrusions', 'action' => 'view', $intrusion['Intrusion']['id'])); ?></p>
 <?php endforeach; ?>
 <?php unset($intrusion); ?>
